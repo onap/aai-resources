1733043036bbf8659db9856eaac583e70f44ed4a
Fiete Ostkamp <Fiete.Ostkamp@telekom.de>
2025-01-06 11:38:34 +0100

    Make aai-resources agnostic of the embedded server (remove jetty-specific code)
    - small adjustments to authentication
    - make users configurable via aai.basic-auth.users[] in application.properties
    - remove Keycloak integration [0]
    
    [0] for the following reasons:
    - integration test (MultiTenancyTest) already not working
    - dependency is ancient (uses 11, 26 is available as of writing)
    - keycloak autoconfiguration is in conflict with spring security
    - keycloak-specific starters have been deprecated and users are advised to
      use the out-of-the-box spring OIDC integration [1]
    - there is no (and likely never will be a) starter that is compatible with spring-boot 3
    
    [1] https://www.keycloak.org/2023/03/adapter-deprecation-update
    
    Issue-ID: AAI-4100
    Change-Id: I8440f44106f9ba2cbd4addfa88efb63329365977
    Signed-off-by: Fiete Ostkamp <Fiete.Ostkamp@telekom.de>

---
+++ aai-resources/src/main/java/org/onap/aai/interceptors/pre/AuthInterceptor.java
@@ -0,0 +1,87 @@
+/**
+ * ============LICENSE_START=======================================================
+ * org.onap.aai
+ * ================================================================================
+ * Copyright © 2025 Deutsche Telekom. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.aai.interceptors.pre;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.regex.Pattern;
+
+import javax.annotation.Priority;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.onap.aai.ResourcesProfiles;
+import org.onap.aai.exceptions.AAIException;
+import org.onap.aai.interceptors.AAIContainerFilter;
+import org.onap.aai.logging.ErrorLogHelper;
+import org.onap.aai.service.AuthorizationService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@PreMatching
+@RequiredArgsConstructor
+@Profile(ResourcesProfiles.ONE_WAY_SSL) // this mainly serves the purpose of making the tests work
+@Priority(AAIRequestFilterPriority.AUTHORIZATION)
+public class AuthInterceptor extends AAIContainerFilter implements ContainerRequestFilter {
+
+  private static final Pattern PATTERN_ECHO = Pattern.compile("^.*/util/echo$");
+  private static final Pattern PATTERN_ACTUATOR = Pattern.compile("^.*/actuator/.*$");
+  private static final AAIException AAI_EXCEPTION = new AAIException("AAI_3300");
+  private final AuthorizationService authorizationService;
+
+  @Override
+  public void filter(ContainerRequestContext requestContext) throws IOException {
+    String path = requestContext.getUriInfo().getRequestUri().getPath();
+    if (PATTERN_ECHO.matcher(path).matches() || PATTERN_ACTUATOR.matcher(path).matches()) {
+      return;
+    }
+
+    String basicAuth = requestContext.getHeaders().getFirst("Authorization");
+    if (basicAuth == null || !basicAuth.startsWith("Basic ")) {
+      Response errorResponse = errorResponse("AAI_3300", requestContext.getAcceptableMediaTypes());
+      requestContext.abortWith(errorResponse);
+      return;
+    }
+
+    if (!authorizationService.isAuthorized(basicAuth)) {
+      Response errorResponse = errorResponse("AAI_3300", requestContext.getAcceptableMediaTypes());
+      requestContext.abortWith(errorResponse);
+      return;
+    }
+  }
+
+  private Response errorResponse(String errorCode, List<MediaType> acceptHeaderValues) {
+
+    return Response
+      .status(AAI_EXCEPTION.getErrorObject().getHTTPResponseCode())
+      .entity(ErrorLogHelper.getRESTAPIErrorResponse(acceptHeaderValues, AAI_EXCEPTION, new ArrayList<>()))
+      .build();
+  }
+
+}
--- aai-resources/src/test/java/org/onap/aai/rest/AuthenticationTest.java
+++ aai-resources/src/test/java/org/onap/aai/rest/AuthenticationTest.java
@@ -18,52 +18,61 @@
  * ============LICENSE_END=========================================================
  */
 
-package org.onap.aai.config;
+package org.onap.aai.rest;
 
 import java.time.Duration;
 import java.util.Collections;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.onap.aai.config.AuthProperties;
+import org.onap.aai.config.WebClientConfiguration;
 import org.onap.aai.setup.SchemaVersions;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-@TestConfiguration
-public class WebClientConfiguration {
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@Import(WebClientConfiguration.class)
+public class AuthenticationTest {
 
+  @Autowired AuthProperties authProperties;
   @Autowired SchemaVersions schemaVersions;
+  @LocalServerPort int port;
 
-  @Lazy
-  @Bean
-  @Primary
-  WebTestClient webTestClient(@LocalServerPort int port) {
-    return WebTestClient.bindToServer()
+  WebTestClient webClient;
+
+  @BeforeEach
+  void setup() {
+    webClient = WebTestClient.bindToServer()
       .baseUrl("http://localhost:" + port + "/aai/" + schemaVersions.getDefaultVersion())
       .responseTimeout(Duration.ofSeconds(300))
       .defaultHeaders(headers -> {
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
         headers.set("X-FromAppId", "test");
         headers.set("X-TransactionId", "someTransaction");
-        headers.setBasicAuth("AAI", "AAI");
       })
       .build();
   }
 
-  @Lazy
-  @Bean
-  WebTestClient mgmtClient(@Value("${local.management.port}") int port) {
-    return WebTestClient.bindToServer()
-      .baseUrl("http://localhost:" + port)
-      .responseTimeout(Duration.ofSeconds(300))
-      .defaultHeaders(headers -> {
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-      })
-      .build();
+  @Test
+  void thatServiceIsAuthenticated() {
+      webClient.get()
+          .uri("/cloud-infrastructure/pservers/pserver/pserver-hostname-test")
+          .exchange()
+          .expectStatus()
+          .isForbidden();
+
+      webClient.get()
+          .uri("/cloud-infrastructure/pservers/pserver/pserver-hostname-test")
+          .headers(headers -> headers.setBasicAuth(authProperties.getUsers().get(0).getUsername(), authProperties.getUsers().get(0).getPassword()))
+          .exchange()
+          .expectStatus()
+          .isNotFound();
   }
+
 }
