fde2ddc51ab05a93c3659d599e61e525e8a7540f
Fiete Ostkamp <Fiete.Ostkamp@telekom.de>
2025-01-06 11:38:34 +0100

    Make aai-common agnostic of the embedded server (remove jetty-specific code)
    - replace custom authorization webfilter with spring security
    - make users configurable via aai.basic-auth.users[] in application.properties
    - remove Keycloak integration [0]
    
    [0] for the following reasons:
    - integration test (MultiTenancyTest) already not working
    - dependency is ancient (uses 11, 26 is available as of writing)
    - keycloak autoconfiguration is in conflict with spring security
    - keycloak-specific starters have been deprecated and users are advised to
      use the out-of-the-box spring OIDC integration [1]
    - there is no (and likely never will be a) starter that is compatible with spring-boot 3
    
    [1] https://www.keycloak.org/2023/03/adapter-deprecation-update
    
    Issue-ID: AAI-4100
    Change-Id: I8440f44106f9ba2cbd4addfa88efb63329365977
    Signed-off-by: Fiete Ostkamp <Fiete.Ostkamp@telekom.de>

--- aai-resources/src/main/java/org/onap/aai/ResourcesApp.java
+++ aai-resources/src/main/java/org/onap/aai/ResourcesApp.java
@@ -42,9 +42,13 @@ import org.springframework.boot.autoconfigure.data.cassandra.CassandraDataAutoCo
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.Profiles;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+@EnableWebSecurity
+@EnableConfigurationProperties
 @SpringBootApplication(
         exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class,
                 HibernateJpaAutoConfiguration.class, CassandraDataAutoConfiguration.class, CassandraAutoConfiguration.class})
